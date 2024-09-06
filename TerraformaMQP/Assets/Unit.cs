230619fa5fcd0838ca1d777b22dd15e415d73255
jdlogan0 <143477762+jdlogan0@users.noreply.github.com>
2024-09-06 02:29:56 -0400

    tileType setup, pathfinding test, generated grid

---
+++ TerraformaMQP/Assets/Unit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    
+    public int tileX = 0;
+    public int tileY = 0;
+
+    public List<Node> path = null;
+
+    public void SetTile(int x, int y) {
+
+    }
+
+}
