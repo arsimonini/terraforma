d3eab6a1f7b527f028c8d9b46d4610dec801a3b9
easweeney01 <103469000+easweeney01@users.noreply.github.com>
2024-09-25 12:07:09 -0400

    Path Arrow Stuff

--- TerraformaMQP/Assets/Map/TileMap.cs
+++ TerraformaMQP/Assets/Map/TileMap.cs
@@ -20,6 +20,10 @@ public class TileMap : MonoBehaviour
 
     //Nodes along the path of shortest path
     public List<Node> currentPath = null;
+    public List<Node> visualPath = null;
+    public GameObject circleArrowPrefab;
+
+    //Sets color to tiles
 
     void Start() {
         GenerateMapData();
@@ -200,7 +204,7 @@ public class TileMap : MonoBehaviour
         {
             if (selectedUnit.GetComponent<Unit>().charSelected || selectedUnit.GetComponent<Enemy_Character_Class>())
             {
-
+                hidePath();
                 generatePathTo(x, y);
                 UnityEngine.Debug.Log(currentPath.Count);
 
@@ -286,6 +290,7 @@ public class TileMap : MonoBehaviour
 
         selectedUnit.GetComponent<Unit>().path = currentPath;
 
+        //showPath();
     }
 
     public bool unitCanEnterTile(int x, int y) {
@@ -314,4 +319,110 @@ public class TileMap : MonoBehaviour
         return dist;
     }
 
+    public void showPath() {
+        hidePath();
+ 
+        //Create path of CircleArrows
+        for (int i = 0; i < visualPath.Count; i++) {
+            GameObject ca = Instantiate(circleArrowPrefab);
+            ca.transform.position = new Vector3(visualPath[i].x,0.6f,visualPath[i].y);
+            ca.transform.localRotation = Quaternion.Euler(90f,0,0);
+            if (i != visualPath.Count - 1) {
+                ca.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            } else {
+                ca.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            }
+        }
+    }
+
+    public void hidePath() {
+        //Delete all instances of CircleArrow
+        GameObject[] existingArrows = GameObject.FindGameObjectsWithTag("CircleArrow");
+        foreach (GameObject arrow in existingArrows) {
+            Destroy(arrow);
+        }
+    }
+    public void visualPathTo(int x, int y){
+
+        if (circleArrowPrefab == null) {
+            UnityEngine.Debug.LogError("circleArrowPrefab has not been assigned in the Inspector!");
+            return;
+        }
+
+        if (selectedUnit.GetComponent<Unit>().tileX == x && selectedUnit.GetComponent<Unit>().tileY == y){
+            visualPath = new List<Node>();
+            selectedUnit.GetComponent<Unit>().path = currentPath;
+            return;
+        }
+
+        selectedUnit.GetComponent<Unit>().path = null;
+        visualPath = null;
+
+        Dictionary<Node, float> dist = new Dictionary<Node, float>();
+        Dictionary<Node, Node> prev = new Dictionary<Node, Node>();
+        Node source = graph[selectedUnit.GetComponent<Unit>().tileX, selectedUnit.GetComponent<Unit>().tileY];
+        Node target = graph[x, y];
+        dist[source] = 0;
+        prev[source] = null;
+
+        //unchecked nodes
+        List<Node> unvisited = new List<Node>();
+
+        foreach (Node n in graph){
+            //Initialize to infite distance
+            if (n != source){
+                dist[n] = Mathf.Infinity;
+                prev[n] = null;
+            }
+            unvisited.Add(n);
+        }
+
+        //if there is a node in unvisited list check it
+        while (unvisited.Count > 0){
+            //unvisited node with shortest distance
+            Node u = null;
+
+            foreach (Node possibleU in unvisited){
+                if (u == null || dist[possibleU] < dist[u]){
+                    u = possibleU;
+                }
+            }
+
+            if (u == target){
+                break;
+            }
+
+            unvisited.Remove(u);
+
+            foreach (Node n in u.neighbors){
+
+                float alt = dist[u] + costToEnterTile(n.x, n.y);
+                if (alt < dist[n]){
+                    dist[n] = alt;
+                    prev[n] = u;
+                }
+            }
+        }
+        if (prev[target] == null){
+            return;
+        }
+        visualPath = new List<Node>();
+        Node curr = target;
+
+        //step through current path and add it to chain
+        while (curr != null){
+            visualPath.Add(curr);
+            curr = prev[curr];
+        }
+        
+        visualPath.Reverse();
+
+        selectedUnit.GetComponent<Unit>().path = currentPath;
+
+        showPath();
+    }
 }
+
+
+
+
--- TerraformaMQP/Assets/Map/Tiles/ClickableTile.cs
+++ TerraformaMQP/Assets/Map/Tiles/ClickableTile.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class ClickableTile : MonoBehaviour
 {
+    //Relevant to lighting up the tile held over
+    private Renderer tileRenderer;
+    private Color originalColor;
+    public float highlightMultiplier = 1.2f; 
+
     public int TileX;
     public int TileY;
     public TileMap map;
@@ -12,10 +17,30 @@ public class ClickableTile : MonoBehaviour
     public List<string> statsToEffect;
     public List<int> effectAmounts;
     
+    void Start() {
+        tileRenderer = GetComponent<Renderer>();
+        originalColor = tileRenderer.material.color;
+    }
+
     void OnMouseUp() {
         map.MoveSelectedUnitTo(TileX, TileY);
     }
 
+    void OnMouseEnter() {
 
+        Color highlightColor = originalColor * highlightMultiplier;
+        tileRenderer.material.color = highlightColor;
+
+        //Highlight Path
+        if (map.selectedUnit != null) {
+            map.visualPathTo(TileX,TileY);
+            //UnityEngine.Debug.Log(currentPath[0].x)
+        }
+    }
+
+    void OnMouseExit() {
+        
+        tileRenderer.material.color = originalColor;
+    }
 
 }
