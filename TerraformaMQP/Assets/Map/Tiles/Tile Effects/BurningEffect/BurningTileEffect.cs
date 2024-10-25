5967864f17dfc0b03984652fb32cebcb7ae85dcb
ThomasBranchaud <tabranchaud@wpi.edu>
2024-10-25 18:20:05 -0400

    Merge fix

--- TerraformaMQP/Assets/Map/Tiles/Tile Effects/BurningEffect/BurningTileEffect.cs
+++ TerraformaMQP/Assets/Map/Tiles/Tile Effects/BurningEffect/BurningTileEffect.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [SerializeField]
 public class BurningTileEffect : tileEffectActions
 {
-    public override void performStartOfTurnEffect(ClickableTile tile){
+    public override void performEndOfTurnEffect(ClickableTile tile){
         UnityEngine.Debug.Log("Proccing Burning");
         if (tile.GetComponent<ClickableTile>().characterOnTile != null){
             tile.GetComponent<ClickableTile>().characterOnTile.GetComponent<Basic_Character_Class>().takeMagicDamage(2, "Fire");
