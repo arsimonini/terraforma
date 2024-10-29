b0a4d19b79f428ef26d615e3107d5610cb654020
ThomasBranchaud <tabranchaud@wpi.edu>
2024-10-29 14:44:03 -0400

    Testing

--- TerraformaMQP/Assets/Spells/LancinSpellList/Fireball/Fireball_Spell.cs
+++ TerraformaMQP/Assets/Spells/LancinSpellList/Fireball/Fireball_Spell.cs
@@ -27,15 +27,27 @@ public class Fireball_Spell : MonoBehaviour, Cast_Spell
             TileEffect newEffect = Instantiate(BurningTileEffect);
             newEffect.createTileEffect(true, targetTile, newSource: "Fireball", newDuration: 3);
             */
+            /* Wall of Fire
+            ClickableTile targetTile = targets[i].GetComponent<ClickableTile>();
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(caster.GetComponent<Basic_Character_Class>().tile.gameObject.transform.position, targetTile.gameObject.transform.position - caster.GetComponent<Basic_Character_Class>().tile.gameObject.transform.position, Vector3.Distance(targetTile.gameObject.transform.position, caster.GetComponent<Basic_Character_Class>().tile.gameObject.transform.position));
+            for (int j = 0; j < hits.Length; j++){
+                targetTile.map.swapTiles(hits[j].collider.gameObject.GetComponent<ClickableTile>(), 1);
+            }
+            */
 
-             
+             ClickableTile targetTile;
             //Normal Casting
-            ClickableTile targetTile = targets[i].GetComponent<Basic_Character_Class>().tile;
+            if (targets[i].GetComponent<Basic_Character_Class>()){
+                targetTile = targets[i].GetComponent<Basic_Character_Class>().tile;
+                targets[i].GetComponent<Basic_Character_Class>().takeMagicDamage(caster.GetComponent<Hero_Character_Class>().magic.moddedValue, "Fire");
+            }
+            else {
+                targetTile = targets[i].GetComponent<ClickableTile>();
+            }
             TileEffect newEffect = Instantiate(BurningTileEffect);
             newEffect.createTileEffect(true, targetTile, newSource: "Fireball", newDuration: 3);
             UnityEngine.Debug.Log(newEffect.duration);
-            targets[i].GetComponent<Basic_Character_Class>().takeMagicDamage(caster.GetComponent<Hero_Character_Class>().magic.moddedValue, "Fire");
-            
         }
     }
 }
