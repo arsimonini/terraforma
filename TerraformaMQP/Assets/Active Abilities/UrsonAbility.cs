dc838a8c795c8ca0674d071f11b69c8eadf76814
ThomasBranchaud <tabranchaud@wpi.edu>
2025-01-27 18:07:54 -0500

    Working Abilities

--- TerraformaMQP/Assets/Active Abilities/UrsonAbility.cs
+++ TerraformaMQP/Assets/Active Abilities/UrsonAbility.cs
@@ -6,5 +6,6 @@ public class UrsonAbility : ActiveAbility, Cast_Spell
 {
     public void castSpell(List<GameObject> targets, GameObject caster){
         targets[0].GetComponent<Basic_Character_Class>().takeMagicDamage(10, "Fire");
+        targets[0].GetComponent<Basic_Character_Class>().updateCharStats();
     }
 }
