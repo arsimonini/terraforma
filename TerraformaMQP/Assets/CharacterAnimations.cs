d2288159c41e27d28e9c57cc98e5dc3a72714721
arsimonini <arsimonini@wpi.edu>
2025-02-19 11:49:39 -0500

    Lancin, Urson, Rold Animations Done

--- TerraformaMQP/Assets/CharacterAnimations.cs
+++ TerraformaMQP/Assets/CharacterAnimations.cs
@@ -40,6 +40,7 @@ public class CharacterAnimations : MonoBehaviour
             ani.SetBool("charIsMovingBack", false); //back
             ani.SetBool("charIsMovingForward", false);
             ani.SetBool("charIsMovingSideways", false);
+            t.localScale = new Vector3(-0.1f, 0.1f, 0.1f);
         }
     }
 
@@ -50,24 +51,31 @@ public class CharacterAnimations : MonoBehaviour
 
         if(camRot == 0) {
             if(startX > x) {
+                //UnityEngine.Debug.Log("X should be more than x: " + startX + "  " + x);
+                t.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                 ani.SetBool("charIsMovingSideways", true); //Left
                 ani.SetBool("charIsMovingBack", false);
                 ani.SetBool("charIsMovingForward", false);
             }
             else if(startX < x) {
+                t.localScale = new Vector3(-0.1f, 0.1f, 0.1f);
+                //UnityEngine.Debug.Log("X should be less than x: " + startX + "  " + x);
                 ani.SetBool("charIsMovingSideways", true); //Right
                 ani.SetBool("charIsMovingBack", false);
                 ani.SetBool("charIsMovingForward", false);
             }
             else if(startZ > z) {
-                ani.SetBool("charIsMovingBack", true); //back
-                ani.SetBool("charIsMovingForward", false);
-                ani.SetBool("charIsMovingSideways", false);
-            }
-            else if(startZ < z) {
+                //UnityEngine.Debug.Log("Z should be less than z: " + startZ + "  " + z);
                 ani.SetBool("charIsMovingForward", true); //forward
                 ani.SetBool("charIsMovingBack", false);
                 ani.SetBool("charIsMovingSideways", false);
+            }
+            else if(startZ < z) {
+                //UnityEngine.Debug.Log("Z should be more than z: " + startZ + "  " + z);
+                ani.SetBool("charIsMovingBack", true); //back
+                ani.SetBool("charIsMovingForward", false);
+                ani.SetBool("charIsMovingSideways", false);
+
             }
             else if(startX == x) {
                 //UnityEngine.Debug.Log("This system won't work");
