7b002dfcc0e41fe4a9e9ed35f32670169021ef62
easweeney01 <103469000+easweeney01@users.noreply.github.com>
2025-02-21 18:47:44 -0500

    Fire Lance Patch

--- TerraformaMQP/Assets/Spells/LancinSpellList/Fire Lance/FireLance_Spell.cs
+++ TerraformaMQP/Assets/Spells/LancinSpellList/Fire Lance/FireLance_Spell.cs
@@ -37,8 +37,8 @@ public class FireLance_Spell : MonoBehaviour, Cast_Spell
         if (targets.Count > 0) { 
             int endX = 0;
             int endY = 0;
-            ClickableTile ct = targets[0].GetComponent<ClickableTile>();
-            if (ct != null) { endX = ct.TileX; endY = ct.TileY;                
+            ClickableTile targetTile = targets[0].GetComponent<ClickableTile>();
+            if (targetTile != null) { endX = targetTile.TileX; endY = targetTile.TileY;                
             } else { //It's likely a character
                 Basic_Character_Class bct = targets[0].GetComponent<Basic_Character_Class>();
                 if (bct != null) {endX = bct.tileX; endY = bct.tileY;}
@@ -47,30 +47,45 @@ public class FireLance_Spell : MonoBehaviour, Cast_Spell
 
             warpath = new List<ClickableTile>();
             TileMap map = basicCaster.map;
+            float startX = basicCaster.transform.position.x;
+            float startZ = basicCaster.transform.position.z;
             
-            int startX = basicCaster.tileX;
-            int startY = basicCaster.tileY;
-            for (int i = 0; i < 6; i++) {
-                int a = (i+1);
-                int b = (6-i);
-                int tweenX = (int) ((float) a*startX+ (float)b*endX)/7; //abbbbbb, aabbbbb, aaabbbb, aaaabbb, aaaaabb, aaaaaab
-                int tweenY = (int) ((float) a*startY+ (float)b*endY)/7;
-                warpath.Add(map.clickableTiles[tweenX,tweenY]);
+            //UnityEngine.Debug.Log("Fire Lance Start Position" + startX.ToString() + ", " + startZ.ToString());
+            float tileTx = targetTile.transform.position.x;
+            float tileTy = targetTile.transform.position.y;
+            float tileTz = targetTile.transform.position.z;
+
+            Vector3 startPosition = new Vector3(startX, tileTy, startZ);
+            Vector3 endPosition = new Vector3(tileTx, tileTy, tileTz);
+
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(startPosition, (endPosition-startPosition).normalized, Vector3.Distance(endPosition,startPosition));
+            UnityEngine.Debug.Log(hits);
+            for (int i = 0; i < hits.Length; i++){
+                GameObject res = hits[i].collider.gameObject;
+                ClickableTile newCt = res.GetComponent<ClickableTile>();
+
+                if (newCt != null) {
+                    placeTileEffect(newCt,"Fire","Fire Lance");
+                }
             }
-            warpath.Add(map.clickableTiles[endX,endY]);
+
+            
+            
+ //           warpath.Add(map.clickableTiles[endX,endY]);
 
 
             //Cull any duplicate values; will do this later
 
-            for (int i = 0; i < warpath.Count; i++) {
+   //         for (int i = 0; i < warpath.Count; i++) {
                 //UnityEngine.Debug.Log("FLAME SPEAR");
-                placeTileEffect(warpath[i],"Fire","Fire Lance");
-            }
+     //           placeTileEffect(warpath[i],"Fire","Fire Lance");
+       //     }
 
             //Hit anyone standing on the last spot.
-            UnityEngine.Debug.Log("Fire Lance End Point:" + warpath[warpath.Count - 1].TileX + ", " + warpath[warpath.Count - 1].TileY);
+            //UnityEngine.Debug.Log("Fire Lance End Point:" + warpath[warpath.Count - 1].TileX + ", " + warpath[warpath.Count - 1].TileY);
             
-            if (warpath[warpath.Count-1].characterOnTile != null) {
+            /*if (warpath[warpath.Count-1].characterOnTile != null) {
                 //Translate them to a basic_character_class
                 Basic_Character_Class characterGettingHit = warpath[warpath.Count - 1].characterOnTile.GetComponent<Basic_Character_Class>();
                 
@@ -79,43 +94,69 @@ public class FireLance_Spell : MonoBehaviour, Cast_Spell
                     characterGettingHit.takeMagicDamage((int) damage,"Fire");
                 }
 
-            }
+            }*/
         }
 
     }
 
     public List<GameObject> displaySpecificAOE(string attackType, ClickableTile centerTile, int size = 0, bool square = false, ClickableTile targetersTile = null){
+        int endX = centerTile.TileX;
+        int endY = centerTile.TileY;
         List<GameObject> list = new List<GameObject>();
         warpath = new List<ClickableTile>();
 
         TileMap map = centerTile.map;
 
-        int endX = centerTile.TileX;
-        int endY = centerTile.TileY;
+        //Get Lancin's tile's position
+        if (targetersTile == null) {return list;}
+        Vector3 startPos = targetersTile.transform.position;
+        Vector3 endPos = centerTile.transform.position;
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(startPos, (endPos-startPos).normalized, Vector3.Distance(endPos,startPos));
+        UnityEngine.Debug.Log(hits);
+        
+        for (int i = 0; i < hits.Length; i++){
+            GameObject res = hits[i].collider.gameObject;
+            ClickableTile newCt = res.GetComponent<ClickableTile>();
+
+            if (newCt != null) {
+                //placeTileEffect(newCt,"Fire","Fire Lance");
+                list.Add(newCt.gameObject);
+                lightTile(list,map,newCt.TileX,newCt.TileY);
+            }
+        }
+
+       // lightTile(list,map,endX,endY);
+
+        //int endX = centerTile.TileX;
+        //int endY = centerTile.TileY;
         
         //Get Lancin's position
-        int startX = targetersTile.TileX;
-        int startY = targetersTile.TileY;
+        //int startX = targetersTile.TileX;
+        //int startY = targetersTile.TileY;
 
         //Find the maximum six points between Lancin and the target
-        for (int i = 0; i < 6; i++) {
+        
+        
+        /*for (int i = 0; i < 6; i++) {
             int a = (i+1);
             int b = (6-i);
 
-            int tweenX = (a*startX+b*endX)/7; //abbbbbb, aabbbbb, aaabbbb, aaaabbb, aaaaabb, aaaaaab
-            int tweenY = (a*startY+b*endY)/7;
+            int tweenX = (int) Mathf.Round((a*startX+b*endX)/7); //abbbbbb, aabbbbb, aaabbbb, aaaabbb, aaaaabb, aaaaaab
+            int tweenY = (int) Mathf.Round((a*startY+b*endY)/7);
 
-            //Add the tile to the posiiton
+            //Add the tile to the position
             warpath.Add(map.clickableTiles[tweenX,tweenY]);
 
             lightTile(list, map, tweenX,tweenY); 
             //centerTile tweenTile = map.clickableTiles[tweenX,tweenY];
             
             //list.Add(tweenTile.gameObject);
-        }
+        }*/
 
-        lightTile(list, map, endX,endY); 
-        warpath.Add(map.clickableTiles[endX,endY]);
+        //lightTile(list, map, endX,endY); 
+        //warpath.Add(map.clickableTiles[endX,endY]);
         return list;
     }
 
